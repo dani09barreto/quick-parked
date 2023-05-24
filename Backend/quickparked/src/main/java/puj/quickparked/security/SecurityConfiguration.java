800413841070c4b94f8417fa168cabd2271a331e
Nicolas Cubillos <nicolasdavidcubillos@gmail.com>
2023-05-24 00:10:31 -0500

    CORS configuration.

--- Backend/quickparked/src/main/java/puj/quickparked/security/SecurityConfiguration.java
+++ Backend/quickparked/src/main/java/puj/quickparked/security/SecurityConfiguration.java
@@ -11,6 +11,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import puj.quickparked.security.jwt.*;
 
 @Configuration
@@ -42,6 +44,17 @@ public class SecurityConfiguration {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200");
+            }
+        };
     }
 }
 
