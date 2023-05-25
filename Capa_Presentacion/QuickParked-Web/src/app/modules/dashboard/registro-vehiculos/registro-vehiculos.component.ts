d0adef9459d85308c15e96b0ab7590d34b22e57b
Juan Páez <jdpluc302@gmail.com>
2023-05-25 11:31:09 -0500

    Arreglado menu despliegue
    Arreglado bug que no se desplegaban los items

--- Capa_Presentacion/QuickParked-Web/src/app/modules/dashboard/registro-vehiculos/registro-vehiculos.component.ts
+++ Capa_Presentacion/QuickParked-Web/src/app/modules/dashboard/registro-vehiculos/registro-vehiculos.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { DashboardModule } from '../dashboard.module';
 import { VehiculosService } from 'src/app/services/vehiculos.service';
 import { tipoVehiculo } from 'src/app/shared/model/tipo.vehiculo';
+
 @Component({
   selector: 'qp-registro-vehiculos',
   templateUrl: './registro-vehiculos.component.html',
@@ -13,23 +14,47 @@ export class RegistroVehiculosComponent {
   placa: string = '';
   cuposDisponibles: string = 'Cupos Disponibles: ';
   cuposReservados: string = 'Cupos Reservados: ';
+  placaTouched: boolean = false;
   tipoVehiculos: tipoVehiculo[] = []
+  materials: string[] = ['Material 1', 'Material 2', 'Material 3'];
+  tipoVehiculoSeleccionado: string = 'Tipo vehiculo';
+  isDropdownOpen: boolean = false;
+  opciones: string[] = []
   vehiculos: any[] | undefined; // Variable para almacenar los datos de los vehículos
+  regex: string = '^[A-Za-z]{3}\\d{3}$'; // Expresión regular por defecto para 'Carro'
 
   ngOnInit(){
     this.vehiculoService.getAllTipoVehiculos().subscribe((tipoVehiculos: tipoVehiculo[]) => {
       this.tipoVehiculos = tipoVehiculos
-      console.log(this.tipoVehiculos)
+      this.opciones = tipoVehiculos.map(opcion => opcion.tipo);
     });
   }
-  accion1():void{
-
+  accion(opcion: string){
+    console.log('Opción seleccionada:', opcion);
   }
-  accion2():void{
-
+  placaValida(): boolean{
+    const regex = new RegExp(this.regex);
+    return regex.test(this.placa);
   }
-  accion3():void{
+  updateRegex(option: string): void {
+    if (option === 'carro') {
+      this.regex = '^[A-Za-z]{3}\\d{3}$'; // Expresión regular para 'Carro'
+    } 
+    if (option === 'moto') {
+      this.regex = '^[A-Z]{3}\\d{2}[A-Z]$'; // Expresión regular para 'Moto'
+    }
+  }
+  
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
 
+  selectTipoVehiculo(material: string) {
+    this.tipoVehiculoSeleccionado = material;
+    this.isDropdownOpen = false;
+  }
+  convertToUppercase() {
+    this.placa = this.placa.toUpperCase();
   }
   ingresarPlaca():void{
 
